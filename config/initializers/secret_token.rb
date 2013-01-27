7830fac4efe7e5c22b9b3d436875bd561ce1007c
Bernardo Galindo <bernardo466@gmail.com>
2013-01-27 09:58:10 -0600

    start asylum web page *non profit*

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CasaHogar::Application.config.secret_token = 'dfce6d0de5c5794d22126eefe38a4539d3f4cec364c0a7f030a59d4701a8c59747fa98efedb5f2f17d4fb0df1a9120b29438fe969f4cbb92ca76f4e525131061'
